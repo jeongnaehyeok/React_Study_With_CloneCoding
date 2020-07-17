1249871012aad053f0b643b16e21f22167a2bf86
jeongnaehyeok <cnh0301@gmail.com>
2020-07-17 13:44:46 +0900

    완성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
 
-function App() {
+import About from './routes/About';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+
+import Navigation from './components/Navigation';
+
+import './App.css';
+
+const App = () => {
   return (
-    <div className="App" />
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/movie-detail" component={Detail}/>
+    </HashRouter>
   );
 }
 
